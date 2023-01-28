4054cfb8cbf1e70b8692ff1900930755469d1d36
deffinad <deffinjr890@gmail.com>
2023-01-28 06:58:06 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { TambahSuratKeluar } from "./Pages/Surat/tambahSuratKeluar";
 import { DetailSuratKeluar } from "./Pages/Surat/detailSuratKeluar";
 import { PengajuanSurat } from "./Pages/Surat/pengajuanSurat";
 import { Profile } from "./Pages/Profile/Profile";
-import { Worke } from "@react-pdf-viewer/core";
+import { Worker } from "@react-pdf-viewer/core";
 
 function App() {
   const { token, setToken } = useToken();
